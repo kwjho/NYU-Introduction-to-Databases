763eebe9d8d093920fd84fcb5493606b25fd0632
Jeffrey Ho <80304515+kwjho@users.noreply.github.com>
2021-03-24 22:01:16 +0800

    Add files via upload

---
+++ Week 3 Querying the Database - Single Table/Lab3-query.sql
@@ -0,0 +1,4 @@
+SELECT product_name, list_price, date_added 
+FROM products 
+WHERE list_price BETWEEN 500 AND 2000 
+ORDER BY date_added DESC;
---
+++ Week 3 Querying the Database - Single Table/Lab7-query.sql
@@ -0,0 +1,4 @@
+SELECT 100 AS price,
+	0.07 AS tax_rate,
+    100 * 0.07 AS tax_amount,
+    100 + 100 * 0.07 AS total;
